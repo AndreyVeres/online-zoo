a7c6f4169da6e94096f53a70514c59cd7feaa218
Andrey <spacecmetanka@gmail.com>
2022-10-06 23:55:51 +0300

    feat:more pets cards

--- online-zoo/pages/donate/scripts.js
+++ online-zoo/pages/donate/scripts.js
@@ -3,17 +3,22 @@ import formInput from "../../assets/js/form.js";
 formInput()
 donateDots()
 
-// ограничивает количество символов в инпуте
-const input = document.querySelector('.donate__form-input');
-input.addEventListener('keydown', () => {
-    if (input.value.length > 3) {
-        input.value = input.value.slice(0, 3)
-    }
+window.addEventListener('DOMContentLoaded' , () => {
+
+    const input = document.querySelector('.donate__form-input');
+    // ограничивает количество символов в инпуте
+    input.addEventListener('keydown', () => {
+        if (input.value.length > 3) {
+            input.value = input.value.slice(0, 3)
+        }
+    })
+    
+    
+    
+    //Активные ссылки меню 
+    document.querySelectorAll('.header__link-active').forEach(item => {
+        item.addEventListener('click', (e) => e.preventDefault())
+    })
+    
 })
 
-
-
-//Активные ссылки меню 
-document.querySelectorAll('.header__link-active').forEach(item => {
-    item.addEventListener('click', (e) => e.preventDefault())
-})
--- online-zoo/pages/main/scripts.js
+++ online-zoo/pages/main/scripts.js
@@ -1,25 +1,25 @@
-
-
 import popup from "../../assets/js/popup.js";
 import rewievCarousel from "../../assets/js/rewievCarousel.js";
 import formInput from "../../assets/js/form.js";
 import petsArrows from "../../assets/js/petsArrows.js"
-
-
 import petsGallery from "../../assets/js/petsGallery.js"
-petsGallery()
+window.addEventListener('DOMContentLoaded', () => {
+
+    petsGallery()
 
 
-petsArrows()
-formInput()
-popup('.testimonials__item', '.popup__wrapper', '.popup', '.popup__close')
-rewievCarousel()
+    petsArrows()
+    formInput()
+    popup('.testimonials__item', '.popup__wrapper', '.popup', '.popup__close')
+    rewievCarousel()
 
-//Активные ссылки меню 
-document.querySelectorAll('.header__link-active').forEach(item => {
-    item.addEventListener('click', (e) => e.preventDefault())
+    //Активные ссылки меню 
+    document.querySelectorAll('.header__link-active').forEach(item => {
+        item.addEventListener('click', (e) => e.preventDefault())
+    })
+
+
 })
-
 
 
 
